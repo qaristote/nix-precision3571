d9cd709714fc3ee016d0df402ab6d8635f2f3886
Quentin Aristote <quentin@aristote.fr>
2021-08-07 11:44:39 +0200

    remove unused options

--- home/config/alacritty.nix
+++ home/config/alacritty.nix
@@ -1,16 +1,11 @@
-{ config, ... }:
+{ ... }:
 
-let
-  user = config.home.username;
-  host = config.networking.hostname;
-in {
+{
   programs.alacritty = {
     enable = true;
 
     settings = {
       window = {
-        title = "${user}@{hostname}";
-
         padding = {
           x = 10;
           y = 10;
