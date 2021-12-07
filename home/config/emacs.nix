65df310cb360613777c165ff3328ac075d4b0fc0
Quentin Aristote <quentin@aristote.fr>
2021-12-07 13:06:27 +0100

    load emacs mode for why3 in ~/.emacs.d

--- home/config/emacs.nix
+++ home/config/emacs.nix
@@ -11,4 +11,6 @@
   };
 
   home.file.".spacemacs.d/init.el".source = ./dotfiles/spacemacs;
+  home.file.".emacs.d/private/layers/why3/local/why3".source =
+    "${pkgs.why3}/share/emacs/site-lisp";
 }
