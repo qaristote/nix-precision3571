30c718bb6c3c38ed3885c5f56d070eaf29ce0ef0
Quentin Aristote <quentin@aristote.fr>
2022-01-04 17:04:47 +0100

    nixos : make busybox available

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -19,7 +19,7 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs; [ vim coreutils openssl ];
+  environment.systemPackages = with pkgs; [ vim busybox coreutils openssl ];
   nixpkgs.config = { allowUnfree = true; };
 
   nix = {
