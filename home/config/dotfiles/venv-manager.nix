8347326a8e729ee4bc2650987c4f73f3b36e5177
Quentin Aristote <quentin@aristote.fr>
2022-01-20 16:17:43 +0100

    venv-manager : latex : add koma as default package

--- home/config/dotfiles/venv-manager.nix
+++ home/config/dotfiles/venv-manager.nix
@@ -4,10 +4,19 @@
   direnv.enable = lib.mkDefault true;
   pinDerivations.enable = lib.mkDefault true;
 
-  latex.latexmk = {
-    enable = lib.mkDefault true;
-    output.pdf.enable = lib.mkDefault true;
-    rc = lib.optional (lib.pathExists ~/.config/latexmkrc) ~/.config/latexmkrc;
+  latex = {
+    packages = tl: {
+      inherit (tl)
+        scheme-basic # scheme
+        koma-script ragged2e everysel footmisc # koma
+      ;
+    };
+    latexmk = {
+      enable = lib.mkDefault true;
+      output.pdf.enable = lib.mkDefault true;
+      rc =
+        lib.optional (lib.pathExists ~/.config/latexmkrc) ~/.config/latexmkrc;
+    };
   };
 
   nix.enable = lib.mkDefault true;
