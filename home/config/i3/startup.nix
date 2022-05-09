bb0c8f827144ec8e3afc6501f6e203645d963836
Quentin Aristote <quentin@aristote.fr>
2022-05-09 13:31:02 +0200

    i3: launch xfce4-power-manager on startup

--- home/config/i3/startup.nix
+++ home/config/i3/startup.nix
@@ -8,12 +8,13 @@ in {
     };
   in (lib.optional config.services.redshift.enable
     (autostart { command = "systemctl --user start redshift"; }))
-++ (lib.optional (background-image != null) (autostart {
+  ++ (lib.optional (background-image != null) (autostart {
     command = "${pkgs.feh}/bin/feh --bg-scale ${background-image}";
   })) ++ (lib.optional config.services.xidlehook.enable
     (autostart { command = "systemctl --user xidlehook.service"; }))
   ++ (lib.optional config.services.emacs.enable
     (autostart { command = "systemctl --user start emacs.service"; })) ++ [
+      (autostart { command = "xfce4-power-manager --daemon"; })
       # Launch frequently used apps
       (autostart { command = "thunderbird"; })
       (autostart { command = "signal-desktop"; })
