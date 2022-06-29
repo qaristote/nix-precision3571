1a5c61955b5fc59039d736edd036cdab56881cfe
Quentin Aristote <quentin@aristote.fr>
2022-06-29 13:29:14 +0200

    home: pkgs: firefox: arkenfox user.js: update to v101

--- home/pkgs/firefoxPackages/default.nix
+++ home/pkgs/firefoxPackages/default.nix
@@ -7,7 +7,7 @@ in {
     inherit (pkgs-rycee.firefox-addons) buildFirefoxXpiAddon;
   };
   arkenfox-userjs = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/arkenfox/user.js/d6b26e75588bcd4311251c68912d3c77b8c6e996/user.js";
+    url = "https://raw.githubusercontent.com/arkenfox/user.js/1a899966a911dc2b69a808095ac7836bef5e214b/user.js";
     sha256 = "DBsQ/Zy0Jd6eanOX6437/SufSVBMumqTq1ZFbIRGWGo=";
   };
 }
