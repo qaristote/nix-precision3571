96d184afebbf34a41c6452b7c3203e4a703beed1
Quentin Aristote <quentin@aristote.fr>
2022-06-29 13:05:40 +0200

    home: gitignore: update hashes

--- home/pkgs/gitignore/default.nix
+++ home/pkgs/gitignore/default.nix
@@ -1,11 +1,15 @@
 { pkgs }:
 
-let fetch-gitignore = module: sha256:
-      let url =  "https://www.toptal.com/developers/gitignore/api/" + module;
-      in pkgs.fetchurl { inherit url sha256; };
+let
+  fetch-gitignore = module: sha256:
+    let url = "https://www.toptal.com/developers/gitignore/api/" + module;
+    in pkgs.fetchurl { inherit url sha256; };
 in {
-  emacs = fetch-gitignore "emacs" sha256:1lswj6cna6y67i9siyr3rsa6z4ky42s9pv8zrji33swdlb8xmzxf;
-  linux = fetch-gitignore "linux" sha256:16f8gr0glc1ai6flm310kv5sfhil6sjh4qr4k1914da1bhhib390;
-  direnv = fetch-gitignore "direnv" sha256:1cwnav4zfd8z078j4s4z1avk23az6d463ysk6jpqmsplz3bq6hyq;
+  emacs = fetch-gitignore "emacs"
+    "sha256:34LaJsGa5fFSMjE7l8JgQAmH8f07jcQmsaOdPVctHMk=";
+  linux = fetch-gitignore "linux"
+    "sha256:Az39kpxJ1pG0T+3KUwx217+f+L8FQEWzwvRFSty8cJU=";
+  direnv = fetch-gitignore "direnv"
+    "sha256:CK47JLrsjf9yyjGAUfhjxLns0r1jDYgSBsp6LN0Yut8=";
   fetcher = fetch-gitignore;
 }
