fa14dc0c7607fa8bce4a15a94995e901a5cde66e
Quentin Aristote <quentin@aristote.fr>
2022-07-19 15:51:17 +0200

    config: firefox: update arkenfox-user.js to v102.1

--- home/pkgs/firefoxPackages/default.nix
+++ home/pkgs/firefoxPackages/default.nix
@@ -8,7 +8,7 @@ in {
   };
   arkenfox-userjs = pkgs.fetchurl {
     url =
-      "https://raw.githubusercontent.com/arkenfox/user.js/ceacc9dd74086478d43ccf3a4f0bd5befda53a43/user.js";
-    sha256 = "sha256:1Gkv8YcFvutNNa/eFQbe+RTSFatTE6uGlfPtzOIPtQI=";
+      "https://raw.githubusercontent.com/arkenfox/user.js/c6ab6c4b4895d533c872085ebb6f3b6c78627d80/user.js";
+    sha256 = "sha256-6oZYIL0HMix/CagZUgePcJU9r1Yz0BWsumEbUwkkwDs=";
   };
 }
