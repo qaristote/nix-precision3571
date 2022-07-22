1035343593326464f60d969d3f350bd8b3f3a324
Quentin Aristote <quentin@aristote.fr>
2022-07-22 10:41:29 +0200

    home: config: picom: refactor to use structural settings

--- home/config/picom.nix
+++ home/config/picom.nix
@@ -4,11 +4,11 @@
   services.picom = {
     enable = true;
     fade = true;
-    activeOpacity = "1";
-    inactiveOpacity = "0.9";
-    menuOpacity = "0.8";
-    shadow = true;
     fadeDelta = 4;
-    blur = true;
+    activeOpacity = 1.0;
+    inactiveOpacity = 0.9;
+    menuOpacity = 0.8;
+    shadow = true;
+    settings.blur.method = "gaussian";
   };
 }
