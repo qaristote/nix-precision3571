783bea9e7d16ffb83d78baa8573aa62c54b5737e
Quentin Aristote <quentin@aristote.fr>
2022-10-06 18:05:57 +0200

    firefox: arkenfox-userjs: update to v105.0

--- home/pkgs/firefoxPackages/default.nix
+++ home/pkgs/firefoxPackages/default.nix
@@ -6,9 +6,10 @@ in {
     inherit (pkgs) fetchurl stdenv lib;
     inherit (pkgs-rycee.firefox-addons) buildFirefoxXpiAddon;
   };
-  arkenfox-userjs = pkgs.fetchurl {
+  arkenfox-userjs = let version = "105.0";
+  in pkgs.fetchurl {
     url =
-      "https://raw.githubusercontent.com/arkenfox/user.js/c6ab6c4b4895d533c872085ebb6f3b6c78627d80/user.js";
-    sha256 = "sha256-6oZYIL0HMix/CagZUgePcJU9r1Yz0BWsumEbUwkkwDs=";
+      "https://raw.githubusercontent.com/arkenfox/user.js/${version}/user.js";
+    sha256 = "sha256:4v5VcCj5h+pLZ7wt38AoWUQgg62qPGohWwmY6yNZZ/0=";
   };
 }
