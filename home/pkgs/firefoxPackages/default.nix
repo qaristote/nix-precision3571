787eacb4004656381fac275e8dd8d4aa7603573b
Quentin Aristote <quentin@aristote.fr>
2023-01-09 18:32:03 +0100

    home: firefox: update to v108

--- home/pkgs/firefoxPackages/default.nix
+++ home/pkgs/firefoxPackages/default.nix
@@ -6,10 +6,10 @@ in {
     inherit (pkgs) fetchurl stdenv lib;
     inherit (pkgs-rycee.firefox-addons) buildFirefoxXpiAddon;
   };
-  arkenfox-userjs = let version = "107.0";
+  arkenfox-userjs = let version = "108.0";
   in pkgs.fetchurl {
     url =
       "https://raw.githubusercontent.com/arkenfox/user.js/${version}/user.js";
-    sha256 = "sha256:470uKF/GHlerqMKym5AedWkipb8PEJgNeQ/l1mDkcdE=";
+    sha256 = "sha256:IxZ2s+efJ4pPORJ+xfZqQUfe1CnYpbGn2QAymRNj/uI=";
   };
 }
