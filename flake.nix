68617c981feb7ef66bd638b592327aace19e1807
Quentin Aristote <quentin@aristote.fr>
2023-02-17 21:25:10 +0100

    format flake

--- flake.nix
+++ flake.nix
@@ -6,15 +6,23 @@
   };
 
   outputs = { self, nixpkgs, my-nixpkgs, nixos-hardware, home-manager }:
-    let system = "x86_64-linux"; in {
-    nixosConfigurations.latitude-7490 = nixpkgs.lib.nixosSystem {
-      inherit system;
-      specialArgs = { inherit nixos-hardware home-manager; home.qaristote = import ./home; };
-      modules = [ my-nixpkgs.nixosModules.personal ./nixos ];
+    let
+      system = "x86_64-linux";
+      overlays-module = { ... }: {
+        nixpkgs.overlays = [ my-nixpkgs.overlays.personal ];
+      };
+    in {
+      nixosConfigurations.latitude-7490 = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = {
+          inherit nixos-hardware home-manager;
+          home.qaristote = import ./home;
+        };
+        modules = [ overlays-module my-nixpkgs.nixosModules.personal ./nixos ];
+      };
+      homeConfigurations.qaristote = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages."${system}";
+        modules = [ overlays-module ./home ];
+      };
     };
-    homeConfigurations.qaristote = home-manager.lib.homeManagerConfiguration {
-      pkgs = nixpkgs.legacyPackages."${system}";
-      modules = [ ./home ];
-    };
-  };
 }
