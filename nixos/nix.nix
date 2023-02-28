c9e2228249c05ef5e8505cc9ade941fe2c2685bd
Quentin Aristote <quentin@aristote.fr>
2023-02-28 19:04:52 +0100

    nixos: update my-nixpkgs on upgrade

--- nixos/nix.nix
+++ nixos/nix.nix
@@ -10,7 +10,7 @@
 
   system.autoUpgrade.flags =
     let update-input = input: [ "--update-input" input ];
-    in update-input "latitude-7490/nixpkgs" ++ update-input "latitude-7490/home-manager" ++ update-input "latitude-7490/nixos-hardware";
+    in update-input "latitude-7490/nixpkgs" ++ update-input "latitude-7490/home-manager" ++ update-input "latitude-7490/nixos-hardware" ++ update-input "latitude-7490/my-nixpkgs";
 
   # make auto-upgrade service lightweight
   systemd.services.nixos-upgrade.unitConfig = { CPUWeight = 1; };
