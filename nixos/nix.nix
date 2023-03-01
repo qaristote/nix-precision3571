d5f0c356a33ec0b5670595f9a5175a642acee900
Quentin Aristote <quentin@aristote.fr>
2023-03-01 14:07:42 +0100

    nixos: nix: upgrade flags: use personal lib

--- nixos/nix.nix
+++ nixos/nix.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   personal.nix = {
@@ -8,9 +8,12 @@
     gc.enable = true;
   };
 
-  system.autoUpgrade.flags =
-    let update-input = input: [ "--update-input" input ];
-    in update-input "latitude-7490/nixpkgs" ++ update-input "latitude-7490/home-manager" ++ update-input "latitude-7490/nixos-hardware" ++ update-input "latitude-7490/my-nixpkgs";
+  system.autoUpgrade.flags = pkgs.personal.lib.updateInputFlags [
+    "latitude-7490/nixpkgs"
+    "latitude-7490/home-manager"
+    "latitude-7490/nixos-hardware"
+    "latitude-7490/my-nixpkgs"
+  ];
 
   # make auto-upgrade service lightweight
   systemd.services.nixos-upgrade.unitConfig = { CPUWeight = 1; };
