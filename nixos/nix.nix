20825c9ec1dee69791d252ac3709cfc5c850045b
Quentin Aristote <quentin@aristote.fr>
2023-03-01 16:22:09 +0100

    use stylix

--- nixos/nix.nix
+++ nixos/nix.nix
@@ -9,10 +9,11 @@
   };
 
   system.autoUpgrade.flags = pkgs.personal.lib.updateInputFlags [
-    "latitude-7490/nixpkgs"
     "latitude-7490/home-manager"
-    "latitude-7490/nixos-hardware"
     "latitude-7490/my-nixpkgs"
+    "latitude-7490/nixos-hardware"
+    "latitude-7490/nixpkgs"
+    "lattitude-7490/stylix"
   ];
 
   # make auto-upgrade service lightweight
