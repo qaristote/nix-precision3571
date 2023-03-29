0281846fabc853266c82caf5ec88a05be6ad41ea
qaristote <quentin.aristote@ens.fr>
2023-03-29 14:17:14 +0200

    nixos: etc: add ens ssl certificate

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -19,4 +19,9 @@
       wlp0s20f3.useDHCP = true;
     };
   };
+
+  environment.etc."ssl/certs/ens.pem".source = pkgs.fetchurl {
+    url = "https://www.tuteurs.ens.fr/internet/USERTrust_RSA_Certification_Authority.pem";
+    sha256 = "sha256:ij28uSqxxid2R/4quFNrXJgqu/2x8d9XKOAbkGq6lTo=";
+  };
 }
