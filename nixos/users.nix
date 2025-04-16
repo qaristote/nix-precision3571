3c7ce2385e699224b5f965022da17d752041c3a6
quentin@aristote.fr <quentin@aristote.fr>
2025-04-16 21:10:07 +0200

    nixos: add plugdev group for adb and pixel 6a

--- nixos/users.nix
+++ nixos/users.nix
@@ -5,5 +5,11 @@
     homeManager.enable = true;
   };
 
+  users.groups.plugdev.members = ["qaristote"];
+  services.udev.extraRules = ''
+    # members of plugdev can use adb on google pixel 6a
+    SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", ATTR{idProduct}=="4ee0", MODE="0666", GROUP="plugdev"
+  '';
+
   home-manager.extraSpecialArgs.osConfig = config;
 }
